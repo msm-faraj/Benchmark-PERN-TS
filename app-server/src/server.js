726a9778ec70ba6e03ecb2541c30acf353be6936
msm-faraj <farajzademeysam@gmail.com>
2023-12-22 18:11:02 +0100

    some bug fixed

--- app-server/src/server.js
+++ app-server/src/server.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const app = express();
 const pool = require("./db");
 const port = 4000;
 
-const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendStatus(200);
+  res.send({ message: "server is on" });
+});
+
+app.get("/dbtest", async (req, res) => {
+  try {
+    const table = await pool.query(
+      "CREATE TABLE test_tb(header VARCHAR(50) NOT NULL, message VARCHAR(50)) "
+    );
+    const rows = await pool.query(
+      "INSERT INTO test_tb (header, message) VALUES ($1, $2)",
+      ["test_db", "Creted"]
+    );
+    const test = await pool.query("SELECT * FROM test_tb");
+    res.json(test.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 
 app.listen(port, () => console.log(`Server has started on port: ${port}`));
